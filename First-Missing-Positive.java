819f80be9b3fd1bc8fb99c1c1ed64e5313dd6f41
Atharva Sawai <atharva.s.official@outlook.com>
2025-09-20 23:53:06 +0530

    [41] [Time Beats: 100.00%] [Memory Beats: 40.84%] - LeetPush

---
+++ First-Missing-Positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Place each number in its correct position if possible
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // swap nums[i] and nums[nums[i] - 1]
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        // After rearrangement, check the first index where nums[i] != i+1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all positions are correct, answer is n+1
+        return n + 1;
+    }
+}
